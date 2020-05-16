667f7287f72e6bb0d9e5818be7051af6a1fcf1ce
Michael King <michael.ac.king@outlook.com>
2020-05-16 16:11:23 -0400

    Implement hover and patrol functionality on the hover platform, dress Level01 with some materials and props

---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class ExtensionMethods
+{
+
+
+    /// <summary>
+    /// Rounds a Vector3 to the given decimal
+    /// </summary>
+    /// <param name="vector3"></param>
+    /// <param name="decimalPlaces"></param>
+    /// <returns></returns>
+    public static Vector3 Round(this Vector3 vector3, int decimalPlaces = 2)
+    {
+        float multiplier = 1;
+        for (int i = 0; i < decimalPlaces; i++)
+        {
+            multiplier *= 10f;
+        }
+
+        return new Vector3(
+            Mathf.Round(vector3.x * multiplier) / multiplier,
+            Mathf.Round(vector3.y * multiplier) / multiplier,
+            Mathf.Round(vector3.z * multiplier) / multiplier
+            );
+    }
+
+    public static Vector2 Round(this Vector2 vector2, int decimalPlaces = 2)
+    {
+        float multiplier = 1;
+        for (int i = 0; i < decimalPlaces; i++)
+        {
+            multiplier *= 10f;
+        }
+
+        return new Vector2(
+            Mathf.Round(vector2.x * multiplier) / multiplier,
+            Mathf.Round(vector2.y * multiplier) / multiplier
+            );
+    }
+}
---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    [Tooltip("Waypoint 0 should be set to the object's starting position")]
+    [SerializeField] private List<Vector3> waypoints;
+
+    [SerializeField]
+    private float maxVel = 15f;
+    
+    [SerializeField]
+    private float toVel = 2.5f;
+
+    [SerializeField]
+    private float maxForce = 40f;
+
+    [SerializeField]
+    private float gain = 5f;
+
+    private Rigidbody rb;
+
+    // Waypoints
+    private int current = 0;
+    private int target;
+
+    private void Start()
+    {
+        target = current + 1;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+        AdjustForGravity();
+    }
+
+    private void Move()
+    {
+        Vector3 distance = waypoints[target] - transform.position;
+        Vector3 tgtVel = Vector3.ClampMagnitude(toVel * distance, maxVel);
+        Vector3 error = tgtVel - rb.velocity;
+        Vector3 force = Vector3.ClampMagnitude(gain * error, maxForce);
+
+        rb.AddForce(force);
+
+        Vector2 posVector2 = new Vector2(transform.position.x, transform.position.z);
+        Vector2 targetVector2 = new Vector2(waypoints[target].x, waypoints[target].z);
+
+        if (posVector2.Round(2) == targetVector2)
+        {
+            StartCoroutine(ArriveAtWaypoint());
+        }
+    }
+
+    private IEnumerator ArriveAtWaypoint()
+    {
+        current = target;
+        target++;
+
+        if (target == waypoints.Count)
+        {
+            target = 0;
+        }
+
+        yield return new WaitForSeconds(1f);
+    }
+
+    private void AdjustForGravity()
+    {
+        float targetY = waypoints[target].y;
+        float yDistance = targetY - transform.position.y;
+        Vector3 antiGrav = new Vector3(0f, yDistance, 0f);
+        //float currentVelY = rb.velocity.y;
+        //Vector3 antiGravityForce = new Vector3(0f, -currentVelY, 0f);
+
+        rb.AddForce(antiGrav, ForceMode.Impulse);
+    }
+}
